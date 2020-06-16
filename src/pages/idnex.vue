2d4fa261fcfb26472f05f44431fc4ffa7b43564e
unknown <80650330@qq.com>
2020-06-17 02:43:32 +0800

    banner

--- src/pages/idnex.vue
+++ src/pages/idnex.vue
@@ -48,8 +48,16 @@
              <div class="swiper-button-next" slot="button-next"></div>
         </swiper>
       </div>
-      <div class="ads-box"></div>
-      <div class="banner"></div>
+      <div class="ads-box">
+        <a :href="'/#/product/'+item.id" v-for="(item, index) in adsList" :key="index">
+          <img :src="item.img" alt="">
+        </a>
+      </div>
+      <div class="banner">
+        <a href="'/#/product/30'">
+          <img src="/imgs/banner-1.png" alt="">
+        </a>
+      </div>
       <div class="product-box"></div>
     </div>
     <service-bar></service-bar>
@@ -128,7 +136,25 @@ export default {
           img:'/imgs/item-box-4.jpg',
           name:'iPhone'
         }],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]
-        ]
+        ],
+      adsList:[
+        {
+          id:33,
+          img:'/imgs/ads/ads-1.png'
+        },
+        {
+          id:48,
+          img:'/imgs/ads/ads-2.jpg'
+        },
+        {
+          id:45,
+          img:'/imgs/ads/ads-3.png'
+        },
+        {
+          id:47,
+          img:'/imgs/ads/ads-4.jpg'
+        },
+      ]
     }
   },
 }
@@ -138,6 +164,7 @@ export default {
 <style lang="scss">
 @import '../assets/scss/config.scss';
 @import '../assets/scss/mixin.scss';
+@import '../assets/scss/base.scss';
 .index{
   .swiper-box{
     .nav-menu{
@@ -216,6 +243,18 @@ export default {
         width: 100%;
       }
     }
+  }
+  .ads-box{
+    @include flex();
+    margin-top: 14px;
+    margin-bottom: 31px;
+    a{
+      width: 296px;
+      height: 167px;
+    }
+  }
+  .banner{
+    margin-bottom: 50px;
   }
 }
 </style>
