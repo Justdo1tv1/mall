c893290b6c842466aa49a61a5cc44a0279e6e7ec
unknown <80650330@qq.com>
2020-06-17 01:07:19 +0800

    servicebar

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -225,6 +225,7 @@ export default {
               box-shadow: 0px 7px 6px 0px rgba(0, 0, 0, 0.11);
               z-index: 10;
               transition:  0.5s;
+              background-color: #fff;
               .product{
                 position: relative;
                 float: left;
--- src/pages/idnex.vue
+++ src/pages/idnex.vue
@@ -1,12 +1,60 @@
 <template>
-  <div>
-    <!-- Index -->
+  <div class="index">
+    <div class="container">
+      <div class="swiper-box">
+        <swiper :options="swiperOption">
+          <swiper-slide v-for="(item, index) in slideList" :key="index">
+            <a :href="'/#/product/'+item.id">
+              <img :src="item.img" alt="">
+            </a>
+          </swiper-slide>
+        </swiper>
+      </div>
+    </div>
+    <service-bar></service-bar>
   </div>
 </template>
 
 <script>
+import {swiper,swiperSlide} from 'vue-awesome-swiper'
+import ServiceBar from '../components/ServiceBar'
+import 'swiper/dist/css/swiper.css'
 export default {
-  name: 'index'
+  name: 'index',
+  components:{
+    ServiceBar,
+    swiper,
+    swiperSlide
+  },
+  data() {
+    return {
+      swiperOption:{
+
+      },
+      slideList:[
+        {
+          id:'42',
+          img:'/imgs/slider/slide-1.jpg'
+        },
+        {
+          id:'45',
+          img:'/imgs/slider/slide-2.jpg'
+        },
+        {
+          id:'46',
+          img:'/imgs/slider/slide-3.jpg'
+        },
+        {
+          id:'42',
+          img:'/imgs/slider/slide-4.jpg'
+        },
+        {
+          id:'42',
+          img:'/imgs/slider/slide-5.jpg'
+        }
+      ]
+    }
+  },
 }
 </script>
 
